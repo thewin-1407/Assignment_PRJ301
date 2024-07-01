55d844e7eac7e81186ca83bc19de982fb66dfc3f
thewin-1407 <thewin1407@gmail.com>
2024-07-02 00:41:08 +0700

    Create Student_Query.sql
    Test code sql for student

---
+++ Demo_Query/Student_Query.sql
@@ -0,0 +1,54 @@
+-- View Courses for Student
+SELECT
+    s.sid AS StudentID,
+    s.sname AS StudentName,
+    c.cname AS CourseName
+FROM
+    students s
+INNER JOIN
+    courses_students cs ON s.sid = cs.sid
+INNER JOIN
+    courses c ON cs.cid = c.cid
+WHERE
+    s.sid = ?
+
+GO
+
+-- View Subject by Courses for Student
+SELECT
+    s.sname AS StudentName,
+    su.subid AS SubjectID,
+    su.subname AS SubjectName
+FROM
+    students s
+INNER JOIN
+    courses_students cs ON s.sid = cs.sid
+INNER JOIN
+    courses c ON cs.cid = c.cid
+INNER JOIN
+    subjects su ON c.subid = su.subid
+WHERE
+    s.sid = ?
+
+GO
+
+-- View Grade by Subject for Student
+SELECT
+    s.sname AS StudentName,
+    su.subname AS SubjectName,
+    a.aname AS AssessmentName,
+    g.score AS Score
+FROM
+    students s
+INNER JOIN
+    grades g ON s.sid = g.sid
+INNER JOIN
+    exams e ON g.eid = e.eid
+INNER JOIN
+    assesments a ON e.aid = a.aid
+INNER JOIN
+    subjects su ON a.subid = su.subid
+WHERE
+    s.sid = ?
+    AND su.subid = ?
+
